f2e7c6225073c3d5aeefe1b59992790f0c1e498d
bartosz121 <bmagiera121@gmail.com>
2024-10-18 20:39:42 +0200

    Add errors

---
+++ cmd/api/resource/common/err/err.go
@@ -0,0 +1,32 @@
+package err
+
+import "net/http"
+
+type Error struct {
+	Error string `json:"error"`
+}
+
+func InternalServerError(w http.ResponseWriter, err []byte) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write(err)
+}
+
+func BadRequest(w http.ResponseWriter, err []byte) {
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write(err)
+}
+
+func Unauthorized(w http.ResponseWriter, err []byte) {
+	w.WriteHeader(http.StatusUnauthorized)
+	w.Write(err)
+}
+
+func MethodNotAllowed(w http.ResponseWriter, err []byte) {
+	w.WriteHeader(http.StatusMethodNotAllowed)
+	w.Write(err)
+}
+
+func UnprocessableEntity(w http.ResponseWriter, err []byte) {
+	w.WriteHeader(http.StatusUnprocessableEntity)
+	w.Write(err)
+}
